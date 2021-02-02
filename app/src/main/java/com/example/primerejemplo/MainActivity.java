9b05d333f123859c5b14137e8f16d0cdfc9dc91d
rcaiza5293 <rcaiza5293@uta.edu.ec>
2021-02-02 12:20:23 -0500

    Mis cambios

--- app/src/main/java/com/example/primerejemplo/MainActivity.java
+++ app/src/main/java/com/example/primerejemplo/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.primerejemplo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText et_base;
+    private EditText et_exponente;
+    private TextView tv_potencia;
+    private Button button_potencia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Encontrar las direcciones de controles de memoria del layout
+        et_base = findViewById(R.id.et_base);
+        et_exponente = findViewById(R.id.et_exponente);
+        tv_potencia = findViewById(R.id.tv_potencia);
+        button_potencia = findViewById(R.id.button_Potencia);
+    }
+
+    //Esto se va a ejecutar cuando se de clic en el boton
+
+    public void clic_potencia(View view){
+        int base = Integer.parseInt(et_base.getText().toString());
+        int exponente = Integer.parseInt(et_exponente.getText().toString());
+        int potencia = 1;
+        for (int i = 1; i<=exponente;i++){
+            potencia *= base;
+        }
+
+        //Mostrar el resultado
+        String resultado = String.valueOf(potencia);
+        tv_potencia.setText(resultado);
     }
 }
---
+++ app/src/main/java/com/example/primerejemplo/Matematicas.java
@@ -0,0 +1,4 @@
+package com.example.primerejemplo;
+
+public class Matematicas {
+}
